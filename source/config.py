a05596bc43c09f865b6ecefe14ae2d7eb08483c2
waratake2000 <waratake.dev2000@gmail.com>
2023-06-13 14:31:42 +0900

    initial commit

--- source/config.py
+++ source/config.py
@@ -1,7 +1,7 @@
 import torch
 
 DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-ROOT_PATH = "/root/source/result"
+ROOT_PATH = "/root/result"
 DATASET_PATH = "/root/dataset/Annotated_High-Resolution_Anime"
 ANNOTATION_DATA = "/root/dataset/Annotated_High-Resolution_Anime/annotations/annotations_1_500.csv"
 TEST_SPLIT = 0.05
