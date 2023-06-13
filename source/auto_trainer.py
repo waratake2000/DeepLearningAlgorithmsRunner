7e0c978e425dc674cabaa4e13b1036b897ac738d
waratake2000 <waratake.dev2000@gmail.com>
2023-06-13 16:56:21 +0900

    run train alghorithm

--- source/auto_trainer.py
+++ source/auto_trainer.py
@@ -18,5 +18,9 @@ print(train_data_len_divisors)
 
 for batch_size in train_data_len_divisors[1:]:
     # for data_aug_factor in range(0,4):
-    command = ["python3", "train.py", "--EPOCHS", "500", "--BATCH_SIZE", f"{batch_size}", "--LR", "0.0001", "--MODEL_FILE", "./models/CommonCnn.py", "--DATA_AUG_FAC", "0"]
-    subprocess.run(command)
+    try:
+        command = ["python3", "train.py", "--EPOCHS", "500", "--BATCH_SIZE", f"{batch_size}", "--LR", "0.0001", "--MODEL_FILE", "./models/CommonCnn.py", "--DATA_AUG_FAC", "0"]
+        subprocess.run(command)
+    except:
+        continue
+
